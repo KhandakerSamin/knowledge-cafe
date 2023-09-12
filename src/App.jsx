c8938f891123bbf94bfefbb9dea35535e9e9481a
Khandaker Samin Yeasar <khandakersaminyeasar@gmail.com>
2023-09-12 13:41:28 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
+
 
 function App() {
   return (
     <>
-      <h1 className='text-6xl'>Knowledge Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
     </>
   )
 }
