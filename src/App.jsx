158eedffdf77a340940dd843e956b31871856e87
Khandaker Samin Yeasar <khandakersaminyeasar@gmail.com>
2023-09-12 14:23:36 +0600

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 
 function App() {
   return (
     <>
+      <main className='container mx-auto'>
       <Header></Header>
+      <div className='md:flex '>
       <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
+      </main>
     </>
   )
 }
